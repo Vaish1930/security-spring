7cb9586252160f48d5de5b2480b7d1a4d86fc476
Vaishnavi Srivastava <srivastavavaishnavi39@gmail.com>
2023-06-20 21:29:51 +0530

    Spring Security Completed

---
+++ src/main/java/com/example/security/LoginController.java
@@ -0,0 +1,20 @@
+package com.example.security;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome.html";
+
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login.html";
+
+    }
+}
